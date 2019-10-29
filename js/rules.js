f98cc1ceeb068081ae231baef06e2c90fe43a54e
Pavel Komarov <komarov.pa.it@gmail.com>
2019-10-29 20:06:52 +0300

    Вытаскивает из общей кучи кода модуль с правилами ограничивающими игру rules

---
+++ js/rules.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+(function () {
+  var VALID_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+  var VALID_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+  var VALID_FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
+  window.rules = {
+    VALID_COAT_COLORS: VALID_COAT_COLORS,
+    VALID_EYES_COLORS: VALID_EYES_COLORS,
+    VALID_FIREBALL_COLORS: VALID_FIREBALL_COLORS
+  };
+})();
--- js/setup.js
+++ js/setup.js
@@ -7,9 +7,6 @@ var KEYCODE_ENTER = 13;
 var MOCKUP_WIZARDS_COUNT = 4;
 var MOCKUP_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var MOCKUP_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var VALID_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var VALID_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-var VALID_FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 
 // Поиск DOM-элементов
@@ -30,15 +27,15 @@ var setupSimilarNode = document.querySelector('.setup-similar');
 
 
 var getRandomValidCoatColor = function () {
-  return window.utilities.getRandomArrayEntry(VALID_COAT_COLORS);
+  return window.utilities.getRandomArrayEntry(window.rules.VALID_COAT_COLORS);
 };
 
 var getRandomValidEyesColor = function () {
-  return window.utilities.getRandomArrayEntry(VALID_EYES_COLORS);
+  return window.utilities.getRandomArrayEntry(window.rules.VALID_EYES_COLORS);
 };
 
 var getRandomValidFireballColor = function () {
-  return window.utilities.getRandomArrayEntry(VALID_FIREBALL_COLORS);
+  return window.utilities.getRandomArrayEntry(window.rules.VALID_FIREBALL_COLORS);
 };
 
 
