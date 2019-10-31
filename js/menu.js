4d850badc3902f353428706f306fe098a5528ea4
Pavel Komarov <komarov.pa.it@gmail.com>
2019-10-31 14:48:06 +0300

    Правки по замечаниям в пр (смотри https://github.com/htmlacademy-javascript/1069099-code-and-magick-18/pull/5)

--- js/menu.js
+++ js/menu.js
@@ -7,6 +7,7 @@
   var setupNode = document.querySelector('.setup');
   var setupCloseNode = setupNode.querySelector('.setup-close');
   var setupUserNameNode = setupNode.querySelector('.setup-user-name');
+  var uploadNode = setupNode.querySelector('.upload');
 
 
   var popupOriginCoords = {};
@@ -60,9 +61,58 @@
     }
   };
 
+  var uploadNodeMouseDownHandler = function (evt) {
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+    var dragged = false;
+
+
+    var documentMouseMoveHandler = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setupNode.style.top = (setupNode.offsetTop - shift.y) + 'px';
+      setupNode.style.left = (setupNode.offsetLeft - shift.x) + 'px';
+    };
+
+    var documentMouseUpHandler = function (upEvt) {
+      var uploadNodeClickHandler = function (clickEvt) {
+        clickEvt.preventDefault();
+        uploadNode.removeEventListener('click', uploadNodeClickHandler);
+      };
+
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', documentMouseMoveHandler);
+      document.removeEventListener('mouseup', documentMouseUpHandler);
+
+      if (dragged) {
+        uploadNode.addEventListener('click', uploadNodeClickHandler);
+      }
+    };
+
+
+    evt.preventDefault();
+    document.addEventListener('mousemove', documentMouseMoveHandler);
+    document.addEventListener('mouseup', documentMouseUpHandler);
+  };
+
 
   setupOpenNode.addEventListener('click', setupOpenClickHandler);
   setupOpenIconNode.addEventListener('keydown', setupOpenIconKeydownEnterHandler);
   setupCloseNode.addEventListener('click', setupCloseClickHandler);
   setupCloseNode.addEventListener('keydown', setupCloseKeydownEnterHandler);
+  uploadNode.addEventListener('mousedown', uploadNodeMouseDownHandler);
 })();
