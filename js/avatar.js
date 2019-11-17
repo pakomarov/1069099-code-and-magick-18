1208345ad733c38b3abf73c7e67d49a1d5ebf416
Pavel Komarov <komarov.pa.it@gmail.com>
2019-11-17 10:26:20 +0300

    Добавляет загрузку аватарки

---
+++ js/avatar.js
@@ -0,0 +1,30 @@
+'use strict';
+
+
+(function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var fileChooser = document.querySelector('.upload input[type=file]');
+  var preview = document.querySelector('.setup-user-pic');
+
+  fileChooser.addEventListener('change', function () {
+    var file = fileChooser.files[0];
+
+    if (file) {
+      var fileName = file.name.toLowerCase();
+
+      var matches = FILE_TYPES.some(function (fileType) {
+        return fileName.endsWith(fileType);
+      });
+
+      if (matches) {
+        var reader = new FileReader();
+
+        reader.addEventListener('load', function () {
+          preview.src = reader.result;
+        });
+
+        reader.readAsDataURL(file);
+      }
+    }
+  });
+})();
